e56b9f1c11f0862404edf52646a35a19bdd4b861
Sydney Renee <sydney@solace.ofharmony.ai>
2026-05-12 16:01:42 -0700

    chore: remove local android sdk bootstrap

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,26 +20,6 @@ configurations.configureEach {
     exclude(group = "com.fleeksoft.io", module = "io")
 }
 
-// Setup Android SDK location and licenses automatically
-val sdkDir = file(".android-sdk")
-val licensesDir = sdkDir.resolve("licenses")
-if (!licensesDir.exists()) licensesDir.mkdirs()
-val licenseFile = licensesDir.resolve("android-sdk-license")
-if (!licenseFile.exists()) {
-    licenseFile.writeText(
-        """
-        8933bad161af4178b1185d1a37fbf41ea5269c55
-        d56f5187479451eabf01fb74abc367c344559d7b
-        24333f8a63b6825ea9c5514f83c2829b004d1fee
-        """.trimIndent()
-    )
-}
-val localProperties: File? = rootProject.file("local.properties")
-if (!localProperties?.exists()!!) {
-    val sdkDirPropertyValue = sdkDir.absolutePath.replace("\\", "/")
-    localProperties.writeText("sdk.dir=$sdkDirPropertyValue")
-}
-
 kotlin {
     applyDefaultHierarchyTemplate()
 
